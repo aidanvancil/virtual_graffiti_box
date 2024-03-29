f6a58312a4d6839308b9fffe8ee6444f1f3076ff
1001-Aidan-Vancil <42700427+aidanvancil@users.noreply.github.com>
2024-03-28 21:25:17 -0700

    chore: update for deployment

--- virtual_graffiti_box/settings.py
+++ virtual_graffiti_box/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z7t*d98-np-+2t4!pco*v+vj4vji=l%%8i5b0v3wh0q@g(k$^0'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', default='local_secret')
 # SECURITY WARNING: don't run with debug tupython manage.py shell -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"
-DEBUG = True
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1'] # '27fc-47-33-18-76.ngrok-free.app', 
+DEBUG = 'RENDER' not in os.environ
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME) 
 #CSRF_TRUSTED_ORIGINS = ['https://27fc-47-33-18-76.ngrok-free.app/']
 #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 TAILWIND_APP_NAME = 'app'
@@ -87,8 +88,7 @@ WSGI_APPLICATION = 'virtual_graffiti_box.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
 
