18ddd99d15ae70f5bd5cbf19f34acea48c9c959b
aidanvancil <42700427+aidanvancil@users.noreply.github.com>
2024-04-01 23:07:06 -0700

    chore: update model to hold uid code

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class Laser(models.Model):
     id = models.CharField(max_length=10, primary_key=True)
     color = models.CharField(max_length=30, default='#777777', null=True)
     size = models.IntegerField(default=10)
-    style = models.CharField(max_length=30, default='Fountain')
+    code = models.IntegerField(unique=True)
 
     class Meta:
         app_label = 'app'
@@ -23,6 +23,7 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     laser = models.ForeignKey(Laser, on_delete=models.SET_NULL, null=True)
+    code = models.IntegerField(unique=True)
 
     class Meta:
         app_label = 'app'
