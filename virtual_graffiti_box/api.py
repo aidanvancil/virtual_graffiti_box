b385d9024d7a9422d98b563e76e42b2f55e42f47
aidanvancil <42700427+aidanvancil@users.noreply.github.com>
2024-04-01 23:04:12 -0700

    feat: cleanup expired codes and settings url

--- virtual_graffiti_box/api.py
+++ virtual_graffiti_box/api.py
@@ -16,10 +16,37 @@ pst_timezone = pytz.timezone('America/Los_Angeles')
 
 generated_codes = {}
 generated_user_ids = {}
+first_time_generation = set()
+
+
+def cleanup_expired_codes():
+    current_time = datetime.now(pst_timezone)
+    expired_user_ids = []
+    for user_id, data in generated_codes.items():
+        if data['expiration_time'] <= current_time:
+            expired_user_ids.append(user_id)
+
+    # Delete expired codes along with associated lasers
+    for user_id in expired_user_ids:
+        del generated_codes[user_id]
+        for code, uid in generated_user_ids.items():
+            if uid == user_id:
+                del generated_user_ids[code]
+                break
+        
+        first_time_generation.remove(code)
+        UserProfile.objects.filter(code=user_id).delete()
+        Laser.objects.filter(code=code).delete()
 
 def generate_code(user_id):
     current_time = datetime.now().astimezone(pst_timezone)
-    code = str(10000 + random.randint(0, 89999))
+    code = None
+    while True:
+        code = str(10000 + random.randint(0, 89999))
+        if code not in first_time_generation:
+            break 
+        else:
+            cleanup_expired_codes()
     expiration_time = current_time + timedelta(minutes=5)
     generated_codes[user_id] = {'code': code, 'expiration_time': expiration_time}
     return str(code), expiration_time
@@ -49,15 +76,35 @@ def validate_code(request, code):
     code = str(code)
     if valid_code(code):
         user_id = generated_user_ids[code]
-        generated_codes[user_id]['expiration_time'] = datetime.now().astimezone(pst_timezone) + timedelta(hours=4)
+        user_expiration_time = generated_codes[user_id]['expiration_time']    
+        current_time = datetime.now(pst_timezone)        
+        expiration_threshold = current_time + timedelta(minutes=30)        
+        if user_expiration_time <= expiration_threshold:
+            user_expiration_time += timedelta(hours=4)
+
+        if code not in first_time_generation:
+            Laser.objects.create(id='Red', code=code)
+            Laser.objects.create(id='Green', code=code)
+            Laser.objects.create(id='Purple', code=code)
+            first_time_generation.add(code)
         return HttpResponse(status=200)
     return HttpResponse(status=400)
 
 def generate_settings_url(first_name, last_name, laser_pointer):
-    laser = Laser.objects.get(id=laser_pointer)
-    UserProfile.objects.create(first_name=first_name, last_name=last_name, laser=laser)
-    base64_user_identifier = base64.b64encode(f"{first_name}_{last_name}_{laser_pointer}".encode('utf-8')).decode('utf-8')
-    url = f"{HOST}/settings/{base64_user_identifier}"
+    print(first_name, last_name, laser_pointer)
+    laser_pointer = laser_pointer.capitalize()
+    url = None
+    try:
+        if first_name and last_name and laser_pointer:
+            print(Laser.objects.all())
+            laser = Laser.objects.get(id=laser_pointer)
+            UserProfile.objects.create(first_name=first_name, last_name=last_name, laser=laser)
+            base64_user_identifier = base64.b64encode(f"{first_name}_{last_name}_{laser_pointer}".encode('utf-8')).decode('utf-8')
+            url = f"{HOST}/settings/{base64_user_identifier}"
+    except Exception as e:
+        print(e)
+        pass
+    print(url)
     return url
 
 def fetch_settings_url(request, code):
@@ -67,7 +114,7 @@ def fetch_settings_url(request, code):
         last_name = request.GET.get('lastname')
         laser_pointer = request.GET.get('laser')
         if valid_code(code):
-            user_id = request.session.get('user_id')
+            user_id = generated_user_ids[code]
             generated_codes[user_id]['expiration_time'] = datetime.now().astimezone(pst_timezone) + timedelta(hours=4)
             url = generate_settings_url(first_name, last_name, laser_pointer)
             if url:
